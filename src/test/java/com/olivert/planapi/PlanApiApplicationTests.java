56b998c4550a5f950ebd833dd6b545ab3f662b41
Oliver Tan <olivert@synnex.com>
2019-11-05 17:37:15 +0800

    init project

---
+++ src/test/java/com/olivert/planapi/PlanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olivert.planapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
